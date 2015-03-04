3cbfa00bd50a5ca1d98f3bff45c60f9d9764aa9d
Laura Del Beccaro <lauradelbeccaro@gmail.com>
2015-03-03 20:53:41 -0800

    Add people properties, clean up API request, etc.

--- mixpanel_data_gen.py
+++ mixpanel_data_gen.py
@@ -16,14 +16,18 @@ except ImportError:
     import simplejson as json
 
 
-users = 10
-events = 15
-from_date = datetime.datetime.today() - datetime.timedelta(days=36)
-to_date = datetime.datetime.today() - datetime.timedelta(days=6)
+users = 100
+event_max = 150
+from_date = datetime.datetime.today() - datetime.timedelta(days=60)
+to_date = datetime.datetime.today()
+
 api_key = "f2d9bb9390fd2ca6435b7518949ac1dd"
 api_secret = "77c474a1b8d6f2909ad4d07af8cf504a"
 token = "7ed1660dcb607670ac10f96d863d5e59"
 
+errors = 0
+people = 0
+events = 0
 
 def random_date(start, end):
 	delta = end - start
@@ -31,38 +35,38 @@ def random_date(start, end):
 	random_second = random.randrange(int_delta)
 	return start + datetime.timedelta(seconds=random_second)
 
-# def api_request(data, params, endpoint):
-# 	data_encoded = b64encode(data)
-# 	params["data"] = data_encoded
-# 	parameters = urllib.urlencode(params)
-# 	request = urllib.urlopen(endpoint + parameters)
+def api_request(data, params, endpoint):
+	data_encoded = b64encode(data)
+	params["data"] = data_encoded
+	parameters = urllib.urlencode(params)
+	# print endpoint + parameters
+	request = urllib.urlopen(endpoint + parameters)
+	if request.read() == 0:
+		errors += 1
+		print request.read()
 
 
 ######## Create event with properties #########
 
 class Event(object):
 
-	def __init__(self, event, props):
+	def __init__(self, event, props, super_props):
 		self.event = event
 		self.props = props
+		self.super_props = super_props
+		self.new_props = self.props[:-1] + ", " + self.super_props[1:]
 
 		endpoint = "http://api.mixpanel.com/import/?"
+		data = '{"event": "' + event + '", "properties": ' + str(self.new_props) + '}'
+		params = {"api_key": api_key}
 
-		data = '{"event": "' + event + '", "properties": ' + str(self.props) + '}'
-
-
-		data_encoded = b64encode(data)
-
-		params = urllib.urlencode({"data": data_encoded, "api_key": api_key})
-		f = urllib.urlopen(endpoint + params)
+		api_request(data, params, endpoint)
 
 ######### Create people profile #########
 
 class Person(object):
 
-	def __init__(self, count):
-		self.count = count
-
+	def __init__(self):
 		genders = ["Female", "Male",]
 		female_firstnames = ["Chloe", "Emily", "Emma", "Olivia", "Jennifer", "Beckie", "Laura", "Brenda", "Danielle", "Ellen", "Sarah", "Hannah", "Lola", "Jessica", "Lily", "Savannah", "Isabella", "Rebecca", "Charlotte", "Ella", "Elizabeth", "Mia", "Abigail", "Zoe", "Lauren", "Grace", "Sophia", "Sam", "Samantha", "Rachel", "Natalie", "Lexi", "Paige", "Alice", "Amanda", "Alyssa", "Lucy", "Vanessa",]
 		male_firstnames = ["Michael", "Jacob", "Bryan", "Brian", "Shawn", "Aaron", "Muhammad", "Daniel", "Jonah", "Liam", "Ryan", "Ali", "James", "Ethan", "Harry", "David", "Kyle", "Will", "Alex", "Luke", "Matt", "Matthew", "Mike", "Joe", "Joseph", "Adam", "Jack", "Tyler", "Kevin", "Austin", "Aiden", "Anthony", "Jackson", "Blake", "Max", "Brandon", "Brendan", "Chris", "Nathan", "Spencer", "Jordan",]
@@ -82,10 +86,8 @@ class Person(object):
 		props = {}
 		super_props = {}
 
-		props["Browser"] = ["Chrome", "Safari", "Firefox", "Opera", "Android Mobile", "BlackBerry", "Chrome iOS", "Internet Explorer", "Mobile Safari", "Mozilla",]
 		props["Search Engine"] = ["Google", "Bing", "Yahoo",]
 		props["Referrer"] = ["$direct", "github.com", "news.ycombinator.com", "stackoverflow.com", "t.co", "thenextweb.com", "bing.com", "mail.google.com", "facebook.com", "google.com", "twitter.com", "techcrunch.com", "venturebeat.com",]
-		props["Device"] = ["Android", "BlackBerry", "Windows Phone", "iPad", "iPhone", "iPod Touch",]
 		props["utm_medium"] = ["Referral", "docs", "email", "twitter",]
 		props["utm_source"] = ["Buffer", "Facebook", "Partner", "Dev Weekly",]
 		for prop, value in props.iteritems():
@@ -93,8 +95,12 @@ class Person(object):
 			super_props[prop] = val
 
 		locations = {"San Francisco":"United States", "Los Angeles":"United States", "San Diego":"United States", "New York":"United States", "Chicago":"United States", "Scottsdale":"United States", "Seattle":"United States", "Dallas":"United States", "Philadelphia":"United States", "Orlando":"United States", "Portland":"United States", "Mexico City":"Mexico", "Cancun":"Mexico", "Rio de Janiero":"Brazil", "Barcelona":"Spain", "Madrid":"Spain", "Sevilla":"Spain", "Beijing":"China", "Shanghai":"China", "Tokyo":"Japan", "London":"UK", "Dublin":"Ireland", "Berlin":"Germany", "Munich":"Germany", "Rome":"Italy", "Florence":"Italy", "Venice":"Italy", "Milan":"Italy", "Paris":"France", "Seoul":"South Korea", "Capetown":"South Africa"}
-		super_props["City"] = (random.choice(locations.keys()))
+		super_props["City"] = random.choice(locations.keys())
 		super_props["Country"] = locations[super_props["City"]]
+
+		devices = {"Android": ["Android Mobile", "Chrome"], "Blackberry": ["Blackberry"], "Windows Phone": ["Internet Explorer"], "iPad": ["Mobile Safari", "Chrome iOS", "Mozilla"], "iPhone": ["Mobile Safari", "Chrome iOS"], "iPod Touch": ["Mobile Safari"]}
+		super_props["Device"] = random.choice(devices.keys())
+		super_props["Browser"] =  random.choice(devices[super_props["Device"]])
 
 		super_props["Gender"] = self.gender
 		super_props["$first_name"] = self.firstname
@@ -103,12 +109,16 @@ class Person(object):
 		super_props["$phone"] = self.phone
 
 		self.super_properties = "mixpanel.register(" + str(super_props)[1:-1] + ")"
-		self.people_properties = super_props
+		self.properties = json.dumps(super_props)
 
 	def people_profile(self):
-		data = '{"$token": "' + token + '", "$distinct_id": ' + self.distinct_id + '", "$set": "' + str(self.people_properties) + '}'
+		data = '{"$token": "' + token + '", "$distinct_id": "' + self.distinct_id + '", "$set": ' + str(self.properties) + '}'
+		params = {}
+		endpoint = "http://api.mixpanel.com/engage/?"
+
+		api_request(data, params, endpoint)
 		
-	def add_events(self):
+	def add_events(self, max):
 		event_list = {
 			"View Page": {
 				"Page": ["Pricing", "Tour", "Contact",], 
@@ -123,56 +133,35 @@ class Person(object):
 			},
 		}
 
-		self.first_event_date = from_date
+		self.first_view_date = random_date(from_date, to_date)
+		self.signup_date = random_date(self.first_view_date, to_date)
 
-		for i in range(random.randint(0, events)):
+		Event(event="View Page", props=json.dumps({"Page": "Landing", "Source": random.choice(event_list["View Page"]["Source"]), "distinct_id": self.distinct_id, "token": token, "time": int(self.first_view_date.strftime("%s"))}), super_props=self.properties)
 
-			print 'Person ' + str(self.count+1) + ', Event ' + str(i+1)
+		weight = random.randint(1, 10)
+		if weight <= 4:
+			Event(event="$signup", props=json.dumps({"distinct_id": self.distinct_id, "token": token, "time": int(self.signup_date.strftime("%s"))}), super_props=self.properties)
 
+		for i in range(max):
 			track = random.choice(event_list.keys())
-			event_date = random_date(from_date, to_date)
+			event_date = random_date(self.signup_date, to_date)
 			event_props = {"distinct_id": self.distinct_id, "token": token, "time": int(event_date.strftime("%s"))}
 
 			for prop, prop_name in event_list[track].iteritems():
 				event_props[prop] = random.choice(prop_name)
 
 			event_props = json.dumps(event_props)
-			Event(event=track, props=event_props)
+			Event(event=track, props=event_props, super_props=self.properties)
 
-			if i == 0:
-				self.first_event_date = event_date
-			elif self.first_event_date > event_date:
-				self.first_event_date = event_date
 
-		self.first_view_date = random_date(from_date - datetime.timedelta(days=5), self.first_event_date)
-		Event(event="View Page", props=json.dumps({"Page": "Landing", "Source": random.choice(event_list["View Page"]["Source"]), "distinct_id": self.distinct_id, "token": token, "time": int(self.first_event_date.strftime("%s"))}))
-
-		weight = random.randint(1, 10)
-		if weight <= 4:
-			signup_date = random_date(self.first_view_date, self.first_event_date).strftime("%s")
-			Event(event="$signup", props=json.dumps({"distinct_id": self.distinct_id, "token": token, "time": int(signup_date)}))
+print "Running...."
 
 for i in range(users):
-	new_person = Person(i)
-	# new_person.people_profile()
-	new_person.add_events()
+	new_person = Person()
+	new_person.people_profile()
+	people += 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	num_events = random.randint(0, event_max)
+	new_person.add_events(num_events)
+	events += num_events
+	print "People: " + str(people) + ", Events: " + str(events) + ", Errors: " + str(errors)
